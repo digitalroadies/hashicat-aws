0d906712e9dd84293fa9cf255eeb8606a17ea39c
Eric Santelices <eric.santelices@mavenwave.com>
2021-02-10 22:25:53 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,15 +1,5 @@
 module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "1.17.0"
-
-resource "aws_s3_bucket" "els-b" {
-    bucket = "my-mwawslab-bucket"
-    acl = "private"
-
-    tags = {
-        Name    = "My AWS Bucket"
-        Environment = "Dev"
-    }
-}
-
+  bucket = "els-s3-mw-awslab"
 }
