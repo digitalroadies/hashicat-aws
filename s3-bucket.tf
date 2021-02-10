8d6a482468e9ae8de4ce7dd8bcf185c6585373fd
Eric Santelices <Ericsante@gmail.com>
2021-02-10 17:12:40 -0500

    removed resource section

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,15 +1,4 @@
 module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "1.17.0"
-
-resource "aws_s3_bucket" "els-b" {
-    bucket = "my-mwawslab-bucket"
-    acl = "private"
-
-    tags = {
-        Name    = "My AWS Bucket"
-        Environment = "Dev"
-    }
-}
-
 }
